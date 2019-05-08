b684d78fbd93e767bb0cab6af2a56291c95cf933
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-05-08 10:53:45 -0400

    true upl, mast docs.

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require ('express');
+const morgan = require('morgan');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user: "postgres",
+      password: "test",
+      id : '',
+      name : '',
+      email : '',
+      database : 'robots'
+    }
+  });
+
+
+  
+
+
+
+
+
+const app = express()
+app.use(morgan('short'))
+
+
+app.get("/", (req, res) => {
+    db.select('*').from('users').then(data=> {
+        res.send(data);
+    });
+})
+
+app.get("/users", (req, res) => {
+    db.select('*').from('users').then(data=> {
+        res.send(data);
+    });
+    // res.send("Nodemon auto updates when I save this file")
+})
+
+app.listen(3000, () => {
+    console.log('Server is up and listening to 3000...')
+})
