a7dc02448ac6259581ecab070da6a57c2b9c1a29
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-05-13 08:34:00 -0400

    heroku db

--- app.js
+++ app.js
@@ -2,19 +2,35 @@ const express = require ('express');
 const morgan = require('morgan');
 const knex = require('knex');
 
-const db = knex({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user: "postgres",
-      password: "test",
-      id : '',
-      name : '',
-      email : '',
-      database : 'robots'
-    }
-  });
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//       host : '127.0.0.1',
+//       user: "postgres",
+//       password: "test",
+//       id : '',
+//       name : '',
+//       email : '',
+//       database : 'robots'
+//     }
+//   });
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
  
 const app = express()
